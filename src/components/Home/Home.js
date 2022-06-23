b3c340253e235f35cf93c9a51e2eb5c6a5a626d0
Mariana Hodniki Davie <marianahodniki1991@gmail.com>
2022-06-22 20:39:10 -0600

    add cdn bootstrap

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,29 @@
 import React, { Component } from 'react';
+import { Navbar } from 'react-bootstrap';
+import { Container} from 'react-bootstrap';
+import { Nav} from 'react-bootstrap';
 
 class Home extends Component {
     render() {
         return (
             <div>
-                <h1>Donna Davie</h1>
+                <Navbar bg="dark" variant="dark">
+
+                    <Container>
+
+                        <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+
+                        <Nav className="me-auto">
+
+                            <Nav.Link href="#home">Home</Nav.Link>
+                            <Nav.Link href="#features">Features</Nav.Link>
+                            <Nav.Link href="#pricing">Pricing</Nav.Link>
+
+                        </Nav>
+
+                    </Container>
+                </Navbar>
+                
             </div>
         );
     }
